615fe9d712bedfcc5d049862a21bffa1bac96c71
michanoku <michael.werker@gmail.com>
2026-04-28 06:41:53 +0000

    fix: Port for render

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.static('public'));
 
-app.listen(8080, '0.0.0.0')
+// app.listen(8080, '0.0.0.0')
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.use(routes);
 
